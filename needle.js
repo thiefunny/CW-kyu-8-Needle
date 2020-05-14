ec8e8ea03e814dbd5751bedf965ca0ef3a8f12d9
Mikołaj Walanus <mikolaj@walanus.com>
2020-05-14 19:38:02 +0000

    po prostu needle.js

--- needle.js
+++
@@ -1,6 +0,0 @@
-function findNeedle(haystack) {
-       
-  return ("found the needle at position " + haystack.indexOf("needle"));
-}
-
-findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
